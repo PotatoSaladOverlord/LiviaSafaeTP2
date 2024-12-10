7fa357698003fcd3d4866e0a13c49fe16c7b809f
Livia Leroy-Stone <livia@Ordinateur-portable-de-Livia.local>
2024-12-10 16:11:40 +0100

    pause

--- exo2-livia-safae/src/app/app.component.ts
+++ exo2-livia-safae/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router'; // Nécessaire pour RouterModule et les routes
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {ListeComponent} from "./liste/liste.component";
 
 import { appRoutes } from './app.routes'; // Import des routes (si ton fichier de routes s'appelle app.routes.ts)
 
@@ -10,7 +11,7 @@ import { appRoutes } from './app.routes'; // Import des routes (si ton fichier d
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent, ListeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

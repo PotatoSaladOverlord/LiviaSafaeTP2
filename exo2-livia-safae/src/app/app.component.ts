310a5cde90b45c4510307bcefc5ac25fae730e9a
Livia Leroy-Stone <livia@Ordinateur-portable-de-Livia.local>
2024-12-10 15:55:02 +0100

    contact cliquable

--- exo2-livia-safae/src/app/app.component.ts
+++ exo2-livia-safae/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router'; // Nécessaire pour RouterModule et les routes
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+
+import { appRoutes } from './app.routes'; // Import des routes (si ton fichier de routes s'appelle app.routes.ts)
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

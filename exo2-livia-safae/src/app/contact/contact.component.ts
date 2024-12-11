964657e55f486fc46a687c2d320e98caebee2d6a
Safae-glitch <campusafae@gmail.com>
2024-12-11 12:16:35 +0100

    contact commencer

--- exo2-livia-safae/src/app/contact/contact.component.ts
+++ exo2-livia-safae/src/app/contact/contact.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    age: null,
+    comment: ''
+  };
+  showEmail = false;  // Pour contrôler l'affichage du champ email
 }
+
